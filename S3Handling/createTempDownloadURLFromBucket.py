14564e1be588ac541c33deff9ab91ea7028b19af
isa sand <isasandh@gmail.com>
2018-01-10 12:36:53 +0100

    send updated object as temporary link in email w boto3

---
+++ S3Handling/createTempDownloadURLFromBucket.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 10 10:08:07 2018
+
+@author: Isa
+"""
+import boto 
+from boto.s3.connection import S3Connection 
+
+AWS_ACCESS_KEY_ID = ''
+AWS_SECRET_ACCESS_KEY = ''
+bucket_name = 'isas-test-bucket'
+
+conn = boto.connect_s3(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY)
+
+bucket = conn.get_bucket(bucket_name)
+listOfKeys = []
+for key in bucket: 
+    listOfKeys.append(key)
+ 
+lastKey = listOfKeys[-1]
+key = lastKey
+
+connection = S3Connection(aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+url = connection.generate_url(
+    60,
+    'GET',
+    bucket_name,
+    key.name,
+    response_headers={
+        'response-content-type': 'application/octet-stream'
+    })
+    
+print(url)
--- lambdaFunctions/eventTriggeredEmail.py
+++ lambdaFunctions/eventTriggeredEmail.py
@@ -2,76 +2,77 @@ import json
 import urllib.parse
 import boto3
 from botocore.exceptions import ClientError
+import botocore 
 
 print('Loading function')
 
 s3 = boto3.client('s3')
-
-
 def lambda_handler(event, context):
     #print("Received event: " + json.dumps(event, indent=2))
 
     # Get the object from the event and show its content type
     bucket = event['Records'][0]['s3']['bucket']['name']
     key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
-    print(event)
+   
     try:
         response = s3.get_object(Bucket=bucket, Key=key)
         print("CONTENT TYPE: " + response['ContentType'])
-        return response['ContentType']
+        #return response['ContentType']
     except Exception as e:
         print(e)
         print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
         raise e
 
-    
-SENDER = ""
-RECIPIENT = ""
-AWS_REGION = "us-east-1"
-SUBJECT = "Amazon SES test"
-BODY_TEXT = "This email was sent from my lambda function with aws SES and boto"
-        
-BODY_HTML = """<html>
-<head></head>
-<body>
-    <h1>Amazon SES Test</h1>
-    <p>This email was sent from my lambda function with 
-        <a href='https://aws.amazon.com/ses/'>Amazon SES</a>
-        and boto</p>
-</body>
-</html>
-"""
-    
-CHARSET = "UTF-8"
-client = boto3.client('ses', region_name = 'us-east-1')
+    bucket_name = "bucket_name"
 
-try: 
-        response = client.send_email( 
-                    Destination={
-                        'ToAddresses': [
-                            RECIPIENT,
-                            ],
-                    },
-                    Message ={
-                        'Body': {
-                            'Html':{
-                                'Charset': CHARSET, 
-                                'Data': BODY_HTML,
+    url = s3.generate_presigned_url('get_object',Params={'Bucket' : bucket_name, 'Key': key,}, ExpiresIn = 86400,)
+    SENDER = ""
+    RECIPIENT = ""
+    AWS_REGION = "us-east-1"
+    SUBJECT = "Amazon SES test"
+    BODY_TEXT = "This email was sent from my lambda function with aws SES and boto"
+        
+    BODY_HTML = """<html>
+    <head></head>
+    <body>
+        <h1>Amazon SES Test</h1>
+        <p>This email was sent from my lambda function with 
+            <a href='https://aws.amazon.com/ses/'>Amazon SES</a>
+            and boto test: {url}</p>
+    </body>
+    </html>
+    """.format(url=url)
+    
+    CHARSET = "UTF-8"
+    client = boto3.client('ses', region_name = AWS_REGION)
+
+    try: 
+            response = client.send_email( 
+                        Destination={
+                            'ToAddresses': [
+                                RECIPIENT,
+                                ],
+                        },  
+                        Message ={
+                            'Body': {
+                                'Html':{
+                                    'Charset': CHARSET, 
+                                    'Data': BODY_HTML,
+                                },
+                                'Text': {
+                                    'Charset': CHARSET, 
+                                    'Data': BODY_TEXT, 
+                                },
                             },
-                            'Text': {
+                            'Subject': {
                                 'Charset': CHARSET, 
-                                'Data': BODY_TEXT, 
+                                'Data': SUBJECT,
                             },
                         },
-                        'Subject': {
-                            'Charset': CHARSET, 
-                            'Data': SUBJECT,
-                        },
-                    },
-                    Source = SENDER, 
-                    )
-except ClientError as e: 
-    print(e.response['Error']['Message'])
-else:   
-    print('Email sent! Message ID: '),
-    print(response['ResponseMetadata']['RequestId'])
+                        Source = SENDER, 
+                        )
+    except ClientError as e: 
+        print(e.response['Error']['Message'])
+    else:   
+        print('Email sent! Message ID: '),
+        print(response['ResponseMetadata']['RequestId'])
